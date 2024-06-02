2a8d3bad1d0a9be9de90fc02d1cc2f141d07b1e7
Bartosz <malytankus@gmail.com>
2024-06-02 13:04:23 +0200

    second committ

--- src/main/java/org/example/auth/Account.java
+++ src/main/java/org/example/auth/Account.java
@@ -1,14 +1,11 @@
 package org.example.auth;
 
-public class Account {
+import org.example.database.DatabaseConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-    int id;
-    String name;
-
-    public Account(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+public record Account(int id, String name) {
 
     public int getId() {
         return id;
@@ -16,4 +13,60 @@ public class Account {
     public String getName() {
         return name;
     }
+
+
+
+    public class AccountManager {
+
+        private DatabaseConnection dbc;
+
+        public AccountManager(DatabaseConnection dbc) {
+            this.dbc = dbc;
+        }
+
+        //Dodawanie użytkownika do bazy danych
+        public Boolean register(Account account, int connectionId) throws SQLException, ClassNotFoundException {
+            String insert = "INSERT INTO accounts VALUES (?,?)";
+            PreparedStatement statement = dbc.connections.get(connectionId).prepareStatement(insert);
+
+            statement.setInt(1,account.id);
+            statement.setString(2, account.name);
+            statement.executeUpdate();
+
+            return true;
+        }
+        //Autentykacja użytkownika
+        public Boolean authenticate(Account account, int connectionId) throws SQLException, ClassNotFoundException {
+            String select = "SELECT * FROM accounts WHERE id = ?";
+            PreparedStatement statement = dbc.connections.get(connectionId).prepareStatement(select);
+            statement.setInt(1, account.id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.getInt(1) == account.id && resultSet.getString(2).equals(account.name)) {
+                System.out.println("Account exists.");
+                return true;
+            }
+
+            System.out.println("Account does not exist.");
+            return false;
+        }
+        //Zwracanie danych użytkownika z bazy danych
+        public Account getAccount(int id, int connectionId) throws SQLException, ClassNotFoundException {
+            String select = "SELECT * FROM accounts WHERE id = ?";
+            PreparedStatement statement = dbc.connections.get(connectionId).prepareStatement(select);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            Account account = new Account(resultSet.getInt(1), resultSet.getString(2));
+
+            if(account.id != 0 && ! account.name.equals(null)) {
+                return account;
+            }
+
+            System.err.println("Error: Account not found.");
+            return null;
+        }
+
+    }
+
+
 }
--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -3,27 +3,28 @@ package org.example.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class DatabaseConnection {
 
-    public Connection connection;
+    public Map<Integer, Connection> connections;
 
-    public Connection getConnection() {
-        return connection;
+    public Connection getConnection(int connectionId) {
+        return connections.get(connectionId);
     }
-    public void connect(String filepath){
+    public void connect(String filepath, int connectionId){
         try {
             String url = "jdbc:sqlite:" + filepath;
-            connection = DriverManager.getConnection(url);
-            if(connection != null){System.out.println("Connected");}
+            Connection connection = DriverManager.getConnection(url);
+            connections.put(connectionId, connection);
+            if(connections.get(connectionId) != null){System.out.println("Connected");}
         }catch (SQLException e){
             e.printStackTrace();
         }
     }
-
-    public void disconnect(int index) throws SQLException {
-        if(connection != null && !connection.isClosed()){
-            connection.close();
+    public void disconnect(int connectionID) throws SQLException {
+        if(connections.get(connectionID) != null && !connections.get(connectionID).isClosed()){
+            connections.get(connectionID).close();
         }
     }
 
